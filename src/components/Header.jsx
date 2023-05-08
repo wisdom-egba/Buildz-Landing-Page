1ab8f4ea6e1edeb8081cea28a6268e6be96d6f90
Wisdom Egba <99222194+wiz-js@users.noreply.github.com>
2023-05-08 15:06:24 +0100

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,34 @@
+import logo1 from "../assets/logo (2).png"
 const Header = () => {
-  return <div></div>
+  return (
+    <div className="text-lg flex bg-[#FFFF]">
+      <div>
+        <img src={logo1} alt="" />
+      </div>
+      <div>
+        <ul className="outline-none uppercase">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Pricing</a>
+          </li>
+          <li>
+            <a href="#">About us</a>
+          </li>
+          <li>
+            <a href="#">Service</a>
+          </li>
+          <li>
+            <a href="#">blog</a>
+          </li>
+          <li>
+            <a href="#">contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default Header
