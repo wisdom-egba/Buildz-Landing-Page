1484525ed5b9513cbf28bd7f0329258a666ad817
Wisdom Egba <99222194+wiz-js@users.noreply.github.com>
2023-05-09 12:17:58 +0100

    updated

--- src/components/Achievement.jsx
+++ src/components/Achievement.jsx
@@ -1,5 +1,68 @@
 import React from "react"
-
+import { HiOutlineLightBulb } from "react-icons/hi"
+import { FaRegMoneyBillAlt } from "react-icons/fa"
+import { BsCreditCard } from "react-icons/bs"
 export const Achievement = () => {
-  return <div>Achievement</div>
+  return (
+    <div className="relative">
+      <div className=" bg-[#000000] flex justify-center ">
+        <div className="text-[#FFFF] mt-[300px]">
+          <h1 className="text-4xl font-semibold flex justify-center py-6">
+            Our Achievements
+          </h1>
+          <p className="text-md lg:text-lg tracking-widest max-w-[55%] m-auto items-center text-center py-5">
+            Buildz is base for future business project which includes all aspect
+            and makes a forecast about your project payback
+          </p>
+          <div className="flex justify-center gap-[100px] mt-9">
+            <div className="flex flex-col justify-center items-center ">
+              <h1 className="text-3xl font-bold py-2">600</h1>
+              <p className="uppercase text-sm">awards won</p>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <h1 className="text-3xl font-bold py-2">567</h1>
+              <p className="uppercase text-sm">satisfied clients</p>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <h1 className="text-3xl font-bold py-2">670</h1>
+              <p className="uppercase text-sm">startup growth</p>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <h1 className="text-3xl font-bold py-2">500</h1>
+              <p className="uppercase text-sm">average prices</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className=" relative z-20 max-w-[85%] md:max-w-[85%] lg:max-w-[70%] m-auto flex justify-between items-center">
+        <div className="bg-[#FFFF] md:h-[200px] md:w-[200px] gap-7 lg:h-[300px] lg:w-[300px] flex justify-center flex-col items-center rounded-lg">
+          <HiOutlineLightBulb
+            size={20}
+            style={{ color: "black", fill: "black" }}
+          />
+          <h1 className="">branding</h1>
+          <p>
+            Regular Business Law Assistance service includes monitoring the
+            legal sphere of your business
+          </p>
+        </div>
+        <div className="bg-[#FFFF] md:h-[200px] md:w-[200px] gap-7 lg:h-[300px] lg:w-[300px] flex justify-center flex-col items-center rounded-lg">
+          <FaRegMoneyBillAlt
+            size={20}
+            style={{ color: "black", fill: "black" }}
+          />
+          <h1 className="">finance</h1>
+          <p>We provide with all financial services including consultation</p>
+        </div>
+        <div className="bg-[#FFFF] md:h-[200px] md:w-[200px] gap-7 lg:h-[300px] lg:w-[300px] flex justify-center flex-col items-center">
+          <BsCreditCard size={20} style={{ color: "black", fill: "black" }} />
+          <h1 className="">marketing</h1>
+          <p>
+            Good marketing plan is crucial for businessmen who wnat to develop
+            their businesses
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,30 @@
 import img1 from "../assets/bro.png"
+
 export const Hero = () => {
   return (
-    <div className=" md:mt-24 md:flex bg-[#FFFF] justify-between items-center max-w-[85%] md:max-w-[85%] lg:max-w-[70%] m-auto">
-      <div className="mt-[90px] md:mt-0 md:w-[60%]">
-        <div className="md:w-[70%]">
-          <h1 className="text-4xl font-bold md:text-2xl lg:text-6xl py-6 tracking-wider ">
-            Making Your Business Ideas
-            <span className="font-extrabold text-4xl"> Come True</span>
-          </h1>
+    <>
+      <div className=" md:mt-24 md:flex bg-[#FFFF] justify-between items-center max-w-[85%] md:max-w-[85%] lg:max-w-[70%] m-auto pb-[200px]">
+        <div className="mt-[90px] md:mt-0 md:w-[60%]">
+          <div className="md:w-[70%]">
+            <h1 className="text-4xl font-bold md:text-2xl lg:text-6xl py-6 tracking-wider ">
+              Making Your Business Ideas
+              <span className="font-extrabold text-4xl"> Come True</span>
+            </h1>
 
-          <p className="py-4 text-lg text-gray-500">
-            We provide businesses with all financial services including
-            financial analysis of the enterprise
-          </p>
-          <button className="heroBtn my-10 py-4 px-10 text-lg md:text-sm">
-            READ MORE
-          </button>
+            <p className="py-4 text-lg text-gray-500">
+              We provide businesses with all financial services including
+              financial analysis of the enterprise
+            </p>
+            <button className="heroBtn my-10 py-4 px-10 text-lg md:text-sm">
+              READ MORE
+            </button>
+          </div>
+        </div>
+
+        <div className="md:w-[50%]">
+          <img src={img1} alt="" />
         </div>
       </div>
-
-      <div className="md:w-[50%]">
-        <img src={img1} alt="" />
-      </div>
-    </div>
+    </>
   )
 }
