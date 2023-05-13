cb7696592f498914ac67f83a4ce5c393b4bccbe1
Wisdom Egba <99222194+wiz-js@users.noreply.github.com>
2023-05-13 22:56:19 +0100

    updated

--- src/components/Achievement.jsx
+++ src/components/Achievement.jsx
@@ -1,35 +1,15 @@
 import React, { useEffect } from "react"
 import { Card } from "./Card"
 import { motion, useAnimation } from "framer-motion"
-import { useInView } from "react-intersection-observer"
 
 export const Achievement = () => {
-  const controls = useAnimation()
-  const [ref, inView] = useInView()
-
-  useEffect(() => {
-    if (inView) {
-      controls.start("visible")
-    }
-  }, [controls, inView])
-  const webVariant = {
-    visible: { opacity: 1, x: 0, transition: { duration: 1 } },
-    hidden: { x: -1000, opacity: 0.4 },
-  }
-
   return (
     <div className="relative ">
       <div className=" bg-[#000000] flex justify-center pb-20 ">
         <div className="text-[#FFFF] mt-[200px]  md:mt-[300px]">
-          <motion.h1
-            ref={ref}
-            initial="hidden"
-            animate={controls}
-            variants={webVariant}
-            className="text-4xl tracking-wide md:text-4xl font-semibold md:flex justify-center py-6 w-[60%] text-center m-auto leading-relaxed"
-          >
+          <h1 className="text-4xl tracking-wide md:text-4xl font-semibold md:flex justify-center py-6 w-[60%] text-center m-auto leading-relaxed">
             Our Achievements
-          </motion.h1>
+          </h1>
           <p className="text-[22px] font-light text-gray-200 md:text-lg tracking-widest md:max-w-[55%] m-auto items-center text-center py-5 px-7 md:px-0 ">
             Buildz is base for future business project which includes all aspect
             and makes a forecast about your project payback
